47dce20d631ef9cab3f837cef1a5f87bc9d01289
Vladyslav Yanchenko <vyanch72@gmail.com>
2022-11-06 22:51:52 +0100

    HW1105final

--- JHW_1105/src/Main.java
+++ JHW_1105/src/Main.java
@@ -3,19 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        MailDeliveryService mds;
-
-        System.out.println("Выбери способ отправки: 1.DHL 2.Email 3.Pigeon ");
-        Scanner console = new Scanner(System.in);
-        int choice = console.nextInt();
-
-        switch (choice) {
-            case (1): mds = new DHL(); break;
-            case (2): mds = new Email(); break;
-            case (3): mds = new Pigeon(); break;
-             default: mds = new DHL();
-        }
-
+        MailDeliveryService mds = new DHL();
+        Sender sender = new Sender();
+        sender.sender(mds);
         mds.sendMail();
     }
 }
---
+++ JHW_1106/src/CreateArray.java
@@ -0,0 +1,3 @@
+public interface CreateArray {
+    void createArray();
+}
